22dfbc19fd012a73cf5293865b7634c179605ba9
Sami Youness <sami.youness@celoxica.com>
2018-11-06 23:19:51 +0100

    [BASH] add a setup script

---
+++ bash/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BACKUP_DIR="back_up"
+
+pushd
+cd ${HOME}
+git clone https://github.com/Bash-it/bash-it.git .bash_it
+mkdir ${BACKUP_DIR}
+if [ -f .bashrc ];
+then
+    mv .bashrc ${BACKUP_DIR}
+    ln -s -f dot-configs/bash/.bashrc
+fi
+
+if [ -f .bash_profile ];
+then
+    mv .bash_profile ${BACKUP_DIR}
+    ln -s -f dot-configs/bash/.bash_profile
+fi
+
+popd
+
+echo "DONE !!"
